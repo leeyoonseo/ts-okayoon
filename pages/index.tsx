64ce7f23bd8bd35e36afef66f16ceece5d69562d
leeyoonseo <okayoon.lee@gmail.com>
2022-01-09 21:05:27 +0900

    기초 프로젝트 설정
    - gitignore 추가
    - 타입스크립트 설정 추가

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+  render() {
+    return (
+      <div>
+        안녕!! 실시간 적용
+      </div>
+    );
+  }
+}
+
+export default index;
