2def3a92676d9767eede19e4d88c653683152323
leeyoonseo <okayoon.lee@gmail.com>
2022-01-23 20:50:19 +0900

    update: app router 연결, guestbook 페이지 생성

---
+++ pages/guestbook.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+const guestbook = () => {
+  return (
+    <>
+      <Head>
+        <title>OKAYOON | 방명록</title>
+      </Head>
+      <div>guestbook</div>
+    </>
+  );
+};
+
+export default guestbook;
