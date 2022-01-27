e0b36bbb69e30ce6206c4df2a86c53524bff3e40
leeyoonseo <okayoon.lee@gmail.com>
2022-01-27 22:41:45 +0900

    update:  useInput hook 추가

---
+++ hooks/useInput.js
@@ -0,0 +1,12 @@
+import { useState, useCallback } from 'react';
+
+const useInput = (initialValue = null) => {
+  const [value, setValue] = useState(initialValue);
+  const handler = useCallback(({ target }) => {
+    setValue(target.value);
+  }, []);
+
+  return [value, handler, setValue];
+};
+
+export default useInput;
